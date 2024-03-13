041b839e89b896976f4c998a10e7812cd5f469e3
emirhanaricii <emirhan190341@gmail.com>
2024-03-14 00:25:46 +0300

    redis configuration handled

--- auth-service/src/main/java/com/customer/auth/config/RedisConfig.java
+++ auth-service/src/main/java/com/customer/auth/config/RedisConfig.java
@@ -1,33 +1,43 @@
 package com.customer.auth.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import redis.clients.jedis.Jedis;
+
+import java.time.Duration;
 
 @Configuration
 public class RedisConfig {
 
-    @Value("${spring.data.redis.host}")
-    private String redisHost;
-    
-    @Value("${spring.data.redis.port}")
-    private int redisPort;
-    
     @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
-        config.setHostName(redisHost);
-        config.setPort(redisPort);
-        return new LettuceConnectionFactory();
+    JedisConnectionFactory jedisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName("localhost");
+        redisStandaloneConfiguration.setPort(6379);
+        redisStandaloneConfiguration.setDatabase(0);
+
+        GenericObjectPoolConfig<Jedis> genericObjectPoolConfig = new GenericObjectPoolConfig<Jedis>();
+        genericObjectPoolConfig.setMaxTotal(10);
+        genericObjectPoolConfig.setMaxIdle(5);
+        genericObjectPoolConfig.setMinIdle(2);
+
+        JedisClientConfiguration.JedisClientConfigurationBuilder jedisClientConfiguration = JedisClientConfiguration.builder();
+        jedisClientConfiguration.connectTimeout(Duration.ofSeconds(1));// 1s connection timeout
+        jedisClientConfiguration.usePooling().poolConfig(genericObjectPoolConfig);
+
+        return new JedisConnectionFactory(redisStandaloneConfiguration,
+                jedisClientConfiguration.build());
     }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory());
+        template.setConnectionFactory(jedisConnectionFactory());
         return template;
     }
 }
